ca544fe39642d1f391d3c346ca6d2e5bac065e0d
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-11 12:46:59 -0700

    added price system and a clock font

---
+++ README.md
@@ -0,0 +1,5 @@
+Style-7 Font:
+	Sizenko Alexander
+	Style-7
+	http://www.styleseven.com
+	Created: October 7 2008
--- Scripts/upgrade_button.gd
+++ Scripts/upgrade_button.gd
@@ -7,9 +7,13 @@ signal upgrade_pressed(upgrade:BaseUpgradeStrategy)
 @export var title_label : RichTextLabel
 @export var desc_label : RichTextLabel
 @export var texture_rect : TextureRect
+@export var price_label : RichTextLabel
 var time_turner : TimeTurner
 var tween : Tween
 var buyable : bool = false
+var level : int = 0
+var time_man : TimeManager
+var scaled_price : float
 
 func manual_init():
 	grab_focus()
@@ -19,12 +23,21 @@ func manual_init():
 	desc_label.text = stored_upgrade.upgrade_description
 	texture_rect.texture = stored_upgrade.texture
 	
-	var time_man = Global.time_manager
+	time_man = Global.time_manager
 	time_man.connect("time_turned_signal", _update_buyable)
 	_update_buyable(0.)
+	
+	update_price()
+
+func update_price():
+	var curr = stored_upgrade.price_in_seconds
+	scaled_price = curr * exp(float(level)/3.)
+	
+	price_label.clear()
+	price_label.append_text("[color=grey][font_size=12]Price: %s" % time_man.format_time_amount(scaled_price))
 
 func _update_buyable(new_total:float):
-	buyable = true if new_total > stored_upgrade.price_in_seconds else false
+	buyable = true if (new_total > scaled_price or is_equal_approx(new_total, scaled_price)) else false
 	if buyable:
 		modulate = Color.WHITE
 	else:
@@ -32,7 +45,9 @@ func _update_buyable(new_total:float):
 
 func _on_pressed() -> void:
 	if buyable:
+		level += 1
 		upgrade_pressed.emit(stored_upgrade)
+		update_price()
 	if tween: tween.kill()
 	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
 	tween.tween_property(self, "scale", Vector2.ONE * 1.1, 0.05)
